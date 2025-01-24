61738adfd787128292ab83c292bc2e6ae1a6bba2
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-01-25 00:15:49 +0100

    added more files...

--- pages/overview_methods.py
+++ pages/overview_methods.py
@@ -5,5 +5,5 @@ st.write(
 """
 I detta avsnitt kommer jag försöka mig på att översiktligt berätta om de metoder för maskininlärning jag kommit över. Jag ska också försöka mig på att kategorisera de.
 """)
-st.image('resources/media/demopictures/5_types_of_regression-f_mobile.png')
+st.image('static/media/demopictures/5_types_of_regression-f_mobile.png')
 st.image('https://doimages.nyc3.cdn.digitaloceanspaces.com/010AI-ML/content/images/2024/02/Screenshot-2024-02-27-at-5.33.37-PM-1.png')
