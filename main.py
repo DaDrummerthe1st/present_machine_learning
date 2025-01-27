e7f823bae73b8dac91286e18d99dacf61abc646c
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-01-27 01:31:17 +0100

    realised the sidebar action appears just by putting the files in pages folder

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
-from fileinput import filename
 
 import pandas as pd
 import streamlit as st
 
-from libraries.database.test_connection import TestConnection
+from libraries.database.connect import TestConnection
 
-# The whole app lives inside a sidebar.
-# Relation to the rest of file system from respective page goes out from main-root
-with st.sidebar:
-    # TODO: relative path to project root
-    # TODO: Cross-platform enabled
-    # https://www.linkedin.com/advice/3/how-do-you-manage-path-differences-python-nwxnf
-    pages = []
-    for (root, subdirs, files) in os.walk('pages/'):
-        for filename in files:
-            pages.append(os.path.splitext(filename)[0])
-    print(pages)
-    for page in pages:
-        st.page_link(page)
+# # The whole app lives inside a sidebar.
+# # Relation to the rest of file system from respective page goes out from main-root
+# with st.sidebar:
+
+#     pages = []
+#     for (root, subdirs, files) in os.walk('pages/'):
+#         for filename in files:
+#             pages.append(os.path.splitext(filename)[0])
+#     print(pages)
+#     for page in pages:
+#         st.page_link(page)
+
+# class CreateSideBar(CollectFiles):
+#     def __init__(self, path):
+#         super().__init__(path)
+#
+#     @staticmethod
+#     def create_sidebar():
+#         with st.sidebar:
+#             st.header('Dessa områden har jag bearbetat')
+#         # with st.sidebar:
+#         #     for page in (os.path.splitext(self.make_list_of_files())[0]):
+#         #         st.page_link(page)
+
+
+
 
 
 class TestRun:
