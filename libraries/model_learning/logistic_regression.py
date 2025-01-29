3fb312d0994e1ef64d2b538860967f4d31c8f6b2
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-01-29 14:49:43 +0100

    gave up on problem solving for now

--- libraries/model_learning/logistic_regression.py
+++ libraries/model_learning/logistic_regression.py
@@ -22,34 +22,45 @@ class ModelTraining:
                         'seems_very_dark',
                         'seems_very_light']
         # X = media represented by pandas Dataframe
-        self.X = None
+        self.X = []
         # y = zero-dimensional binary representation of users way of usage (delete / keep)
-        self.y = None
+        self.y = pd.Series()
 
         # instantiate variables for object-scope
         self.fig = self.ax = self.cnf_matrix = None
         self.X_train = self.X_test = self.y_train = self.y_test = None
         self.y_pred = None
+        # print("init: " + str((self.y, self.X, self.y_pred,self.X_train,self.X_test,self.X_train,self.X_test)))
 
-    def create_X_and_Y(self):
-        """
-        This method createss a pandas DataFrame X and Y columns
-        with categories as headlines
+    def create_X(self, n=10000):
+        """This method creates a Pandas DataFrame representing n number of pictures
+         with categories as headlines"""
 
-        y can be created either as the last column and separated with
-        """
         # Create dummy data representation of image files
-        self.X = pd.DataFrame(np.random.random(size=(10000, 10)),
+        self.X = pd.DataFrame(np.random.random(size=(n, 10)),
                               columns=self.categories)
 
-        # create an answer array (y)
-        self.y = pd.Series((np.ndarray((10000,))), dtype='int8')
+        print(self.X.tail)
+        return self.X
 
-        # Does it work?
-        print(self.y)
-        print(self.X)
+    # def create_Y(self):
+    #     """
+    #     This method creates a Pandas DataFrame X and Y columns
+    #     with categories as headlines
+    #
+    #     y can be created either as the last column and separated with
+    #     """
+    #     print("is this even visible?")
+    #
+    #
+    #
+    #     # Does it work?
+    #     print(type(self.y))
+    #     print(self.y)
+    #     print(self.X)
+    #     yield self.X, self.y
 
-    def create_answers(self):
+    def create_answers(self): # TODO: does not work½½½
         """
         This is a dummy data creator based on the contents of X dataframe
 
@@ -59,18 +70,30 @@ class ModelTraining:
         # additional possible categories for y[index] = True
         # and random_array[index][2] == 1 and random_array[index][3] < 0.3
 
-        for index, answer in enumerate(y):
-            if self.X[index][0] < 0.2:
-                self.y[index] = 1
-            else:
-                self.y[index] = 0
+        print('first_print')
+        print(self.y.tail)
 
-    def split_data(self):
+        # for index, answer in enumerate(self.y):
+        #     if self.X[index][0] < 0.2:
+        #         self.y[index] = 1
+        #     else:
+        #         self.y[index] = 0
+        print(self.X['is_blurry'])
+        for row in (self.X.loc[self.X['is_blurry'] > 0.7]):
+            self.y[row].append(1)
+        else:
+            self.y[row].append(0)
+
+        #self.y = self.X.loc[self.X['is_blurry'] < 0.2]
+        print('visible?')
+        print(self.y.tail)
+
+    def split_data(self): # TODO: not imlemented
         # split X and y data training and testing sets
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             self.X, self.y, test_size=0.25, random_state=16)
 
-    def run_log_reg(self):
+    def run_log_reg(self): # TODO: not implemented
         """Running my first ever logistic regression"""
         logreg = LogisticRegression(random_state=16)
         logreg.fit(self.X_train, self.y_train)
@@ -85,7 +108,7 @@ class ModelTraining:
         plt.xticks(tick_marks, class_names)
         plt.yticks(tick_marks, class_names)
 
-    def create_heatmap(self):
+    def create_heatmap(self): # TODO: not implemented
         """Heatmap displays a visual representation of accuracy"""
         sns.heatmap(pd.DataFrame(
             self.cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
@@ -94,3 +117,7 @@ class ModelTraining:
         plt.title('Confusion matrix', y=1.1)
         plt.ylabel('Actual label')
         plt.xlabel('Predicted label')
+
+# model = ModelTraining()
+# model.create_X()
+# model.create_answers()
