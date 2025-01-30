7816ed705c6d52d4e0b1905affa78e3e69a9998e
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-01-30 03:02:36 +0100

    language corrections

--- pages/overview_methods.py
+++ pages/overview_methods.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('Maskininlärning Översikt')
 st.write(
 """
-I detta avsnitt kommer jag försöka mig på att översiktligt berätta om de metoder för maskininlärning jag kommit över. Jag ska också försöka mig på att kategorisera de.
+I detta avsnitt kommer jag försöka mig på att översiktligt berätta om de metoder för maskininlärning jag kommit över. Jag ska också försöka mig på att kategorisera dem.
 """)
 st.image('static/media/demopictures/5_types_of_regression-f_mobile.png')
 st.image('https://doimages.nyc3.cdn.digitaloceanspaces.com/010AI-ML/content/images/2024/02/Screenshot-2024-02-27-at-5.33.37-PM-1.png')
