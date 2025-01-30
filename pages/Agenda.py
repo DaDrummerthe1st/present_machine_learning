ee07873ea4ffc29bb880b637e632ca3bf67e3bd8
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-01-30 10:16:28 +0100

    changed mission of file

---
+++ pages/Agenda.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('Agenda')
+st.markdown("""
+- Processen
+  - Hittade inget dataset
+  - Machine Learning!
+  - DPfAS 
+
+Min presentation är en work in progress
+""")
+
